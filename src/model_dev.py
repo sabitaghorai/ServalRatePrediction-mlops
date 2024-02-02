8cc4e7457a08075daf26c6a07b121038dadb358f
sabitaghorai <108499262+sabitaghorai@users.noreply.github.com>
2024-02-02 11:58:42 +0530

    Update model_dev.py

--- src/model_dev.py
+++ src/model_dev.py
@@ -1,6 +1,8 @@
 import logging
 from abc import ABC, abstractmethod
 from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
 import pandas as pd
 
 class Model(ABC):
@@ -18,3 +20,25 @@ class LogisticRegressionModel(Model):
         except Exception as e:
             logging.error(f"Error in training the model: {e}")
             raise e
+        
+class RandomForestModel(Model):
+    def train(self, X_train: pd.DataFrame, Y_train: pd.Series, **kwargs):
+        try:
+            clf = RandomForestClassifier(**kwargs)
+            clf.fit(X_train, Y_train)
+            logging.info("Random Forest model training completed")
+            return clf
+        except Exception as e:
+            logging.error(f"Error in training the Random Forest model: {e}")
+            raise e
+
+class SVMModel(Model):
+    def train(self, X_train: pd.DataFrame, Y_train: pd.Series, **kwargs):
+        try:
+            clf = SVC(**kwargs)
+            clf.fit(X_train, Y_train)
+            logging.info("SVM model training completed")
+            return clf
+        except Exception as e:
+            logging.error(f"Error in training the SVM model: {e}")
+            raise e        
